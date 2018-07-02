12486c804f34f697b99047c13385c95f7cd76674
Ian Norden <iannordenn@gmail.com>
2018-07-01 20:52:51 -0500

    plugged in my limiter and added boilerplate structs for various exchange elements

--- restapi.go
+++ restapi.go
@@ -3,86 +3,88 @@
 package main
 
 import (
-	"flag"
+	"time"
 	"log"
 	"net/http"
-	"time"
   "sync"
+	"os"
 
   "github.com/gorilla/mux"
-  "golang.org/x/time/rate"
+  "github.com/i-norden/golimiter"
 )
 
-// Create a custom visitor struct which holds the rate limiter for each
-// visitor and the last time that the visitor was seen.
-type visitor struct {
-    limiter  *rate.Limiter
-    lastSeen time.Time
+
+// Exchange structs
+
+type exchange struct {
+	users []user
+	markets []market
 }
 
-//create
-var limiter = rate.NewLimiter(1, 6)
-
-// Create a map to hold the visitor structs for each ip
-var visitors = make(map[string]*visitor)
-var mtx sync.Mutex
-
-// Create a new rate limiter and add it to the visitors map, using the
-// IP address as the key.
-func addVisitor(ip string) *rate.Limiter {
-    limiter := rate.NewLimiter(2, 5)
-    mtx.Lock()
-    // Include the current time when creating a new visitor.
-    visitors[ip] = &visitor{limiter, time.Now()}
-    mtx.Unlock()
-    return limiter
+type market struct {
+	sync.Mutex
+	pair string
+	orderbook struct {
+		bids []level
+		asks []level
+	}
 }
 
-// Retrieve and return the rate limiter for the current visitor if it
-// already exists. Otherwise call the addVisitor function to add a
-// new entry to the map.
-func getVisitor(ip string) *rate.Limiter {
-    mtx.Lock()
-    v, exists := visitors[ip]
-    if !exists {
-        mtx.Unlock()
-        return addVisitor(ip)
-    }
-    // Update the last seen time for the visitor.
-    v.lastSeen = time.Now()
-    mtx.Unlock()
-    return v.limiter
+type level struct {
+	rate float64
+	totalQuantity float64
+	orders	[]limitOrder
 }
 
-// Every minute check the map for visitors that haven't been seen for
-// more than 3 minutes and delete the entries.
-func cleanupVisitors() {
-    for {
-        time.Sleep(time.Minute)
-        mtx.Lock()
-        for ip, v := range visitors {
-            if time.Now().Sub(v.lastSeen) > 3*time.Minute {
-                delete(visitors, ip)
-            }
-        }
-        mtx.Unlock()
-    }
+type limitOrder struct {
+	time time.Time
+	side string
+	rate float64
+	quantity float64
+	orderID string
+	conditions map[string]interface{}
 }
 
-//checks incoming ip against their limit
-func limit(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        // Call the getVisitor function to retreive the rate limiter for
-        // the current user.
-        limiter := getVisitor(r.RemoteAddr)
-        if limiter.Allow() == false {
-            http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
-            return
-        }
-
-        next.ServeHTTP(w, r)
-    })
+type marketOrder struct {
+	time time.Time
+	side string
+	quantity float64
+	orderID string
+	conditions map[string]interface{}
 }
+
+// User structs
+
+type user struct {
+	sync.Mutex
+	account account
+}
+
+type account struct {
+	id string
+	balances map[string]balances
+	orders map[string]limitOrder // map of orderIDs to the corresponding order
+	orderHistory struct {
+		limiterOrders []limitOrder
+		marketOrders  []marketOrder
+	}
+}
+
+type balances struct {
+	available float64
+	unavailable float64
+	total float64
+}
+
+// Management structs
+
+type management struct {
+	sync.Mutex
+	activeProcesses map[string]*os.Process
+}
+
+
+// Http handler functions for API endpoints
 
 func TokenPairs(w http.ResponseWriter, r *http.Request) {
 
@@ -112,11 +114,9 @@ func Order(w http.ResponseWriter, r *http.Request) {
 
 }
 
-var activeProcesses = make(map[string]bool)
+// Main process
 
 func main() {
-	flag.Parse()
-	log.SetFlags(0)
   r := mux.NewRouter()
   r.Methods("GET", "POST")
 	r.HandleFunc("/v0/token_pairs", TokenPairs)
@@ -126,10 +126,14 @@ func main() {
 	r.HandleFunc("/v0/orderbook", Orderbook)
   r.HandleFunc("/v0/fees", Fees)
   r.HandleFunc("/v0/order", Order)
-	go log.Fatal(http.ListenAndServe(":8080", limit(r)))
-}
-
-// Run a background goroutine to remove old entries from the visitors map.
-func init() {
-    go cleanupVisitors()
+	lim := golimiter.Limiter{}
+	lim.Rate = 1
+	lim.Burst = 5
+	err := lim.Init()
+	if err != nil {
+		log.Fatal("Unable to initiate api limiter")
+	}
+	for {
+		go log.Fatal(http.ListenAndServe(":8080", lim.LimitHTTPHandler(r)))
+	}
 }
